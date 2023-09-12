658a3e021df098ee5e6c666ab17ba81297f4109d
Luciano Barletta <lushobarlett@gmail.com>
2023-09-12 18:53:43 -0300

    TP1 - Anda el eval2!

--- tp1/src/Eval2.hs
+++ tp1/src/Eval2.hs
@@ -14,17 +14,19 @@ type State = M.Map Variable Int
 -- Estado nulo
 -- Completar la definición
 initState :: State
-initState = undefined
+initState = M.empty
 
 -- Busca el valor de una variable en un estado
 -- Completar la definición
 lookfor :: Variable -> State -> Either Error Int
-lookfor v s = undefined
+lookfor v s = case M.lookup v s of
+  Just x  -> Right x
+  Nothing -> Left UndefVar
 
 -- Cambia el valor de una variable en un estado
 -- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update = M.insert
 
 -- Evalua un programa en el estado nulo
 eval :: Comm -> Either Error State
@@ -41,9 +43,70 @@ stepCommStar c    s = do
 -- Evalua un paso de un comando en un estado dado
 -- Completar la definición
 stepComm :: Comm -> State -> Either Error (Pair Comm State)
-stepComm = undefined
+
+stepComm Skip s = return $ Skip :!: s
+
+stepComm (Let v e) s = do
+  n :!: s' <- evalExp e s
+  return $ Skip :!: (update v n s')
+
+stepComm (Seq Skip c2) s = return $ c2 :!: s
+stepComm (Seq c1 c2) s = do
+  c1' :!: s' <- stepComm c1 s
+  return $ (Seq c1' c2) :!: s'
+
+stepComm (IfThenElse bexp c1 c2) s = do
+  b :!: s' <- evalExp bexp s
+  return $ if b then c1 :!: s' else c2 :!: s'
+
+stepComm (Repeat c bexp) s = return $ newcomm :!: s
+  where
+    newcomm = (Seq c (IfThenElse bexp Skip repeat))
+    repeat = Repeat c bexp
 
 -- Evalua una expresion
 -- Completar la definición
+evalUnOp :: (a -> b) -> Exp a -> State -> Either Error (Pair b State)
+evalUnOp op e s = do
+  n :!: s' <- evalExp e s
+  return $ (op n) :!: s'
+
+evalBinOp :: (a -> b -> c) -> Exp a -> Exp b -> State -> Either Error (Pair c State)
+evalBinOp op e1 e2 s = do
+  n1 :!: s' <- evalExp e1 s
+  n2 :!: s'' <- evalExp e2 s'
+  return $ (op n1 n2) :!: s''
+
 evalExp :: Exp a -> State -> Either Error (Pair a State)
-evalExp = undefined
+-- enteras
+evalExp (Const n) s     = return $ n :!: s
+evalExp (Var v) s       = do
+  n <- lookfor v s
+  return $ n :!: s
+evalExp (UMinus e) s    = evalUnOp negate e s
+evalExp (Plus e1 e2) s  = evalBinOp (+) e1 e2 s
+evalExp (Minus e1 e2) s = evalBinOp (-) e1 e2 s
+evalExp (Times e1 e2) s = evalBinOp (*) e1 e2 s
+evalExp (Div e1 e2) s   = do
+  n2 :!: s' <- evalExp e2 s
+  if n2 == 0
+    then Left DivByZero
+    else evalBinOp div e1 e2 s
+evalExp (EAssgn v e) s  = do
+  n :!: s' <- evalExp e s
+  let s'' = update v n s'
+   in return $ n :!: s''
+evalExp (ESeq e1 e2) s  = do
+  _ :!: s'  <- evalExp e1 s
+  n :!: s'' <- evalExp e2 s'
+  return $ n :!: s''
+-- booleanas
+evalExp BTrue s       = return $ True :!: s
+evalExp BFalse s      = return $ False :!: s
+evalExp (Lt e1 e2) s  = evalBinOp (<) e1 e2 s
+evalExp (Gt e1 e2) s  = evalBinOp (>) e1 e2 s
+evalExp (And e1 e2) s = evalBinOp (&&) e1 e2 s
+evalExp (Or e1 e2) s  = evalBinOp (||) e1 e2 s
+evalExp (Not e) s     = evalUnOp not e s
+evalExp (Eq e1 e2) s  = evalBinOp (==) e1 e2 s
+evalExp (NEq e1 e2) s = evalBinOp (/=) e1 e2 s
