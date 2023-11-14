0401c779b81ddbb51c7a67b8445f7f06813ccfbc
Luciano Barletta <lushobarlett@gmail.com>
2023-11-14 00:52:46 -0300

    QC - algun progreso con main

--- final/app/Main.hs
+++ final/app/Main.hs
@@ -1,36 +1,37 @@
 module Main (main) where
 
 import Parser (qcparser, lexer)
-import Common (showState)
+import Common (showState, State)
 import PrettyPrint (prettyPrint)
 import Eval (eval, EvalT(..), defaultRunEnv)
-import System.Console.Haskeline (runInputT, defaultSettings, getInputLine, outputStrLn)
+import System.Console.Haskeline (InputT, runInputT, defaultSettings, getInputLine, outputStrLn)
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.Trans (MonadTrans(lift))
 
-printAST s = outputStrLn $ prettyPrint $ qcparser $ lexer s
+printAST :: String -> IO ()
+printAST = print . prettyPrint . qcparser . lexer
 
-evalIO s = do
-  let result = eval $ qcparser $ lexer s
-  case result of
-    Left err -> outputStrLn err
-    Right state -> outputStrLn $ showState state
+parseAndEval :: String -> EvalT State
+parseAndEval = eval . qcparser . lexer
 
 main :: IO ()
 main = runInputT defaultSettings loop
-  where
-    loop = do
-      input <- getInputLine "λ "
-      case input of
-        Nothing -> return ()
-        Just s -> do
-          if length (words s) == 1
-            then evalIO s
-            else let [command, file] = take 2 $ words s
-                  in case command of
-                    ":f" -> do
-                      sfile <- liftIO $ readFile file
-                      evalIO sfile
-                    ":p" -> do
-                      sfile <- liftIO $ readFile file
-                      printAST sfile
-          loop
+
+loop :: InputT IO ()
+loop = do
+  input <- getInputLine "λ "
+  case input of
+    Nothing -> return ()
+    Just s -> do
+      if length (words s) == 1
+        then lift $ return (showState <$> parseAndEval s)
+        else executeCommand s
+      loop
+
+executeCommand :: String -> InputT IO (EvalT String)
+executeCommand s =
+  let [command, file] = take 2 $ words s
+  in case command of
+    ":f" -> lift $ readFile file >>= \s -> print $ showState <$> parseAndEval s
+    ":p" -> lift $ readFile file >>= printAST
+    _ -> outputStrLn "Unknown command"
